afa7ab0feb34a9502b5ca3d9343279f03537691a
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-01-02 08:21:40 -0700

    init commit

--- src/clear-content.js
+++
@@ -1,6 +0,0 @@
-export function clearContent(){
-    const content = document.getElementById('content');
-    while (content.firstChild) {
-        content.removeChild(content.firstChild);
-    }
-}
