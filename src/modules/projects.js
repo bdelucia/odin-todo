871077a17814adf0c3d84720aebdd02c69e1e207
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-01-04 15:14:53 -0700

    yaya

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,12 +1,20 @@
 let projects = [];
 
-export function Project(name) {
-    return { name }
+function Project(id, name) {
+    return {
+        id, 
+        name,
+        tasks: [],
+    }
 }
 
 export function addProject(name){
-    const newProject = Project(name);
+    const newProject = Project(projects.length+1, name);
     projects.push(newProject);
+}
+
+export function addTasktoProject(projectID, task){
+    
 }
 
 export function printProjects() {
