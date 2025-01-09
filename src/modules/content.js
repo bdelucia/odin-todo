aa7e6a1b0e735e30d1f32e6848bdc5549e5578b2
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-01-09 16:16:24 -0700

    formatting

--- src/modules/content.js
+++ src/modules/content.js
@@ -1,17 +1,21 @@
 import { getProject, projects, selectedProject } from "./projects.js";
+import { createElement } from "./addTask.js";
 export function renderTasks() {
     const tasksContainer = document.getElementById("tasksContainer");
+    const content = document.getElementById('content');
+    const existingh3 = content.querySelector('h3');
+    content.removeChild(existingh3);
     tasksContainer.innerHTML = "";
     const projectToShow = getProject(selectedProject);
 
     if (selectedProject) {
         console.log('Creating DOM elements for tasks');
-        const title = document.createElement("h3");
-        title.textContent = `Tasks for project: ${selectedProject}`;
-        tasksContainer.appendChild(title);
+        const newh3 = createElement('h3');
+        newh3.textContent = `Tasks for project: ${selectedProject}`;
+        content.insertBefore(newh3, content.firstChild);
 
         projectToShow.tasks.forEach(task => {
-            const taskItem = document.createElement("div");
+            const taskItem = createElement('div', {class: 'taskItem'});
             taskItem.textContent = task.title;
             tasksContainer.appendChild(taskItem);
         });
